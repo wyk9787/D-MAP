7459bfa7325ead634e25e91ab695e7356349b1e0
Shuyi Qi <qishuyi@cs.grinnell.edu>
2018-04-25 20:59:10 -0500

    Modify worker

--- worker.cpp
+++ worker.cpp
@@ -11,7 +11,10 @@
 #include <arpa/inet.h>
 
 #define PORT_NUMBER 60519
-#define WORKER_JOIN -1
+#define WORKER_JOIN "-1"
+
+typedef int (*real_main_t)(int argc, char** argv);
+const char* shared_library = "../shared_library/d-map-injection.so";
 
 int socket_connect(char * server_address, int port) {
   // Connect to its parent using directory server
@@ -50,28 +53,42 @@ int main(int argc, char** argv) {
   }
   char* server_address = argv[1];
   int server_socket = socket_connect(server_address, PORT_NUMBER);
-  int server_socket_copy = dup(server_socket);
-  if(server_socket_copy == -1) {
-    perror("dup failed in directory_request");
-    exit(EXIT_FAILURE);
+
+  char msg[3];
+  msg[0] = '1';
+  msg[1] = '\n';
+  msg[2] = '\0';
+  printf(msg);
+  write(server_socket, msg, strlen(msg));
+
+  char buffer[256];
+  int bytes_read = read(s, buffer, 256);
+  if(bytes_read < 0) {
+    perror("read failed");
+    exit(2);
   }
 
-   // Open the socket as a FILE stream so we can use fgets
-  FILE* input_dir = fdopen(server_socket, "r");
-  FILE* output_dir = fdopen(server_socket_copy, "w");
+  int input = atoi(buffer);
 
-  // Check for errors
-  if(input_dir == NULL || output_dir == NULL) {
-    perror("fdopen failed in directory_request");
-    exit(EXIT_FAILURE);
+  errno = 0;
+  void* injection = dlopen(shared_library, RTLD_LAZY | RTLD_GLOBAL);
+  if(injection == NULL) {
+    perror("dlopen");
+    exit(1);
+  }
+  dlerror();
+  real_main_t real_main = (real_main_t)dlsym(injection, "entrance");
+
+  real_main(1, input);
+  char* error = dlerror();
+  if(error != NULL) {
+    printf("Error: %s\n", error);
+    exit(1);
   }
 
-  fprintf(output_dir, "%d\n", WORKER_JOIN);
-  fprintf(output_dir, "lol I\"m connecting");
-  fflush(output_dir);
-
+  
+  
   //close
   close(server_socket);
-  close(server_socket_copy);
   //end
 }
